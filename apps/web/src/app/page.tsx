d2002771b3b53bc7315c988cbc80ec22db217f31
Kodossou <pierremariekod@gmail.com>
2024-11-04 01:42:38 +0000

    Add alert, avatar, badge, button for react

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../react/components/core/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline">Download</Button>
+    </div>
+  );
+}
