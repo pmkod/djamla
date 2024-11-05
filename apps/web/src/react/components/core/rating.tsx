81691f8afc6a162a5b89af0507879e456d23ee85
Kodossou <pierremariekod@gmail.com>
2024-11-05 15:11:42 +0000

    Rating done

---
+++ apps/web/src/react/components/core/rating.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { forwardRef } from "react";
+import { RatingGroup as RatingGroupPrimitives } from "@ark-ui/react";
+import { IconStarFilled } from "@tabler/icons-react";
+import { cva, VariantProps } from "class-variance-authority";
+
+const ratingStarStyle = cva("cursor-pointer outline-none transition-colors", {
+  variants: {
+    size: {
+      sm: "size-5",
+      md: "size-6",
+      lg: "size-7",
+    },
+    highlighted: {
+      true: "opacity-100 fill-amber-400",
+      false: "fill-neutral-200",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    highlighted: true,
+  },
+});
+
+const Rating = forwardRef<
+  React.ElementRef<typeof RatingGroupPrimitives.Root> &
+    VariantProps<typeof ratingStarStyle>,
+  React.ComponentPropsWithoutRef<typeof RatingGroupPrimitives.Root> &
+    VariantProps<typeof ratingStarStyle>
+>(({ children, size, ...props }, ref) => (
+  <RatingGroupPrimitives.Root ref={ref} {...props}>
+    <RatingGroupPrimitives.Control className="flex gap-x-1">
+      <RatingGroupPrimitives.Context>
+        {({ items }) =>
+          items.map((item) => (
+            <RatingGroupPrimitives.Item key={item} index={item}>
+              <RatingGroupPrimitives.ItemContext>
+                {({ highlighted }) => (
+                  <IconStarFilled
+                    fill="current"
+                    className={ratingStarStyle({ highlighted, size })}
+                  />
+                )}
+              </RatingGroupPrimitives.ItemContext>
+            </RatingGroupPrimitives.Item>
+          ))
+        }
+      </RatingGroupPrimitives.Context>
+      <RatingGroupPrimitives.HiddenInput />
+    </RatingGroupPrimitives.Control>
+  </RatingGroupPrimitives.Root>
+));
+
+export { Rating };
