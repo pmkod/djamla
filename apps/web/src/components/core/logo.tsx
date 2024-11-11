db4c633a3d28397c7b39b5c73e8913ac4e04f186
Kodossou <pierremariekod@gmail.com>
2024-11-11 02:53:31 +0000

    Footer done

--- apps/web/src/components/core/logo.tsx
+++ apps/web/src/components/core/logo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Logo = () => {
-  return <div className="text-xl font-medium text-base-800">Djamla</div>;
+  return <div className="text-xl font-bold text-base-600">Djamla</div>;
 };
--- apps/web/src/components/partials/footer.tsx
+++ apps/web/src/components/partials/footer.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { Container } from "../core/container";
+import { Logo } from "../core/logo";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="flex h-52 justify-between">
-      <Container>Footer</Container>
+    <footer className="bg-base-100 border-t border-t-base-300">
+      <Container>
+        <div className="pt-10 pb-12">
+          <div className="mb-4">
+            <Logo />
+          </div>
+          <p className="text-base-500">
+            A project create by{" "}
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/pmkod"
+              className="border-b border-b-base-400"
+            >
+              pmkod
+            </Link>
+          </p>
+          <p className="text-base-500">
+            The source code is available on{" "}
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/pmkod/djamla"
+              className="border-b border-b-base-400"
+            >
+              Github
+            </Link>
+          </p>
+        </div>
+      </Container>
     </footer>
   );
 };
