abdbb3f5646d6ccd399e199a388ddc11ec54e0e5
Kodossou <pierremariekod@gmail.com>
2024-11-25 16:44:20 +0000

    Edit remix dark mode guide

--- playgrounds/remix/app/routes/_index.tsx
+++ playgrounds/remix/app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex items-center justify-center">
       <Button>Aa</Button>
       <Button variant="outline">Aa</Button>
       <ThemeDropdown />
